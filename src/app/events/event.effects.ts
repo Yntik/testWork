39ca5d1a61ab1d83941bbda3299311ff14cc33a1
Alexander Ivashchenko <alexander.ivashchenko@clockwise.software>
2020-06-08 22:12:50 +0300

    add event effect

---
+++ src/app/events/event.effects.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core'
+
+import { Effect, Actions, ofType } from '@ngrx/effects'
+import { map, switchMap, catchError, withLatestFrom } from 'rxjs/operators'
+import { Store } from '@ngrx/store'
+import { of } from 'rxjs'
+
+import { EventsState, Event } from '../store/models'
+import * as EventActions from './event.actions'
+
+import { HttpEventService } from '../services/http-service/http-event.service'
+
+@Injectable()
+export class EventEffects {
+  constructor(private actions$: Actions, private store: Store<EventsState>, private eventHttp: HttpEventService) {}
+  @Effect()
+  UploadProject$ = this.actions$.pipe(
+    ofType(EventActions.GetEvents),
+    switchMap(() => {
+        return this.eventHttp.getEvents().pipe(
+          map((events: Event[]) => {
+            return EventActions.SetEvents({payload: events})
+          }),
+          catchError((error) => {
+            return of(EventActions.ApiError({ payload: error }))
+          })
+        )
+    }),
+  )
+}
